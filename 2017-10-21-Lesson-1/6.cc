dfa1125b0f4c18ab53b9adc1b77f2e9ffc015109
karinakozarova <karinakozarova1303@gmail.com>
2017-10-21 12:37:47 +0300

    more exercises

--- 2017-10-21-Lesson-1/6.cc
+++ 2017-10-21-Lesson-1/6.cc
@@ -9,14 +9,18 @@ using namespace std;
 int main() {
 	int n,count = 0;
 	cin >> n;
+	int copy = n;
 	int arr[n];
+
 	while(n--){
 		cin >> arr[n];
 	}
-	sort(arr, arr+n); 
-   	cout << arr;
-  	for(int i =0; i< n; i++){
 
+	sort(arr, arr+copy); 
+
+  	for(int i =0; i< n; i++){
+  		if(arr[i] == arr[i+1]) count++;
   	}
+  	cout << count;
     return 0;
 }
