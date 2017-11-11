280bf7ea1f51edd8ee949b6327f6f043d505f111
karinakozarova <karinakozarova1303@gmail.com>
2017-11-11 12:39:44 +0200

    add first task

---
+++ 2017-11-11-Lesson-2/num.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main(){
+	long long int n,k;
+
+	cin >> n >> k; // stoinost
+
+	long long l = 0,r = n,a_n,result = 0;
+
+	while(r-l > 1){
+		int middle = (l+r)/2;
+		a_n = pow(middle,1.3) + 3*pow(middle,0.7) + 9*pow(middle,0.3); 
+		if(a_n == k) {
+			result = middle; 
+			break;
+		} else{
+			if(pow(middle,1.3) + 3*pow(middle,0.7) + 9*pow(middle,0.3) <= k) l = middle;
+			else r = middle;
+		}
+	}
+
+	if(result == 0) cout << -1;
+	else cout << result;
+
+	return 0;
+}
